7064ce43ac267bcb4aa95df9b25cb5f54484ac12
raeeschachar <raees.chachar@arbisoft.com>
2017-02-25 17:18:05 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,12 +2,6 @@
 #=========================================================
 
 #=========================================================
-echo "Install the packages..."
-#=========================================================
-sudo apt-get update
-sudo apt-get -y install fluxbox xorg unzip vim default-jre rungetty firefox
-
-#=========================================================
 echo "Set autologin for the Vagrant user..."
 #=========================================================
 sudo sed -i '$ d' /etc/init/tty1.conf
@@ -26,67 +20,30 @@ echo "${PROFILE_STRING}" >> .profile
 echo "ok"
 
 #=========================================================
-echo "Download the latest chrome..."
-#=========================================================
-wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo rm google-chrome-stable_current_amd64.deb
-sudo apt-get install -y -f
-
-#=========================================================
-echo "Download latest selenium server..."
-#=========================================================
-SELENIUM_VERSION=$(curl "https://selenium-release.storage.googleapis.com/" | perl -n -e'/.*<Key>([^>]+selenium-server-standalone[^<]+)/ && print $1')
-wget "https://selenium-release.storage.googleapis.com/${SELENIUM_VERSION}" -O selenium-server-standalone.jar
-chown vagrant:vagrant selenium-server-standalone.jar
-
-#=========================================================
-echo "Download latest chrome driver..."
-#=========================================================
-CHROMEDRIVER_VERSION=$(curl "http://chromedriver.storage.googleapis.com/LATEST_RELEASE")
-wget "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
-sudo rm chromedriver_linux64.zip
-chown vagrant:vagrant chromedriver
-
-#=========================================================
-echo -n "Install tmux scripts..."
-#=========================================================
-TMUX_SCRIPT=$(cat <<EOF
-#!/bin/sh
-tmux start-server
-
-tmux new-session -d -s selenium
-tmux send-keys -t selenium:0 './chromedriver' C-m
-
-tmux new-session -d -s chrome-driver
-tmux send-keys -t chrome-driver:0 'java -jar selenium-server-standalone.jar' C-m
-EOF
-)
-echo "${TMUX_SCRIPT}"
-echo "${TMUX_SCRIPT}" > tmux.sh
-chmod +x tmux.sh
-chown vagrant:vagrant tmux.sh
-echo "ok"
-
-#=========================================================
-echo -n "Install startup scripts..."
-#=========================================================
-STARTUP_SCRIPT=$(cat <<EOF
-#!/bin/sh
-~/tmux.sh &
-xterm &
-EOF
-)
-echo "${STARTUP_SCRIPT}" > /etc/X11/Xsession.d/9999-common_start
-chmod +x /etc/X11/Xsession.d/9999-common_start
-echo "ok"
-
-#=========================================================
 echo -n "Add host alias..."
 #=========================================================
 echo "192.168.33.1 host" >> /etc/hosts
 echo "ok"
+
+#=========================================================
+echo -n "Install Pip"
+#=========================================================
+sudo apt-get install python-setuptools python-dev build-essential -y
+sudo easy_install pip 
+
+#=========================================================
+echo -n "Install Firefox v46"
+#=========================================================
+sudo apt-get remove firefox
+wget https://sourceforge.net/projects/ubuntuzilla/files/mozilla/apt/pool/main/f/firefox-mozilla-build/firefox-mozilla-build_45.0.2-0ubuntu1_amd64.deb
+sudo dpkg -i firefox-mozilla-build_45.0.2-0ubuntu1_amd64.deb
+sudo apt-mark hold firefox
+
+#=========================================================
+echo -n "Install Git"
+#=========================================================
+sudo apt-get update
+sudo apt-get install git -y
 
 #=========================================================
 echo "Reboot the VM"
